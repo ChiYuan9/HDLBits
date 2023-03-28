448d5cdf00a1a64709adad75b1b848ba584da8f3
ChiYuan9 <108168201+ChiYuan9@users.noreply.github.com>
2023-03-28 19:52:43 -0400

    Update 1_rule90.v

--- Circuits/Sequential Logic/More Circuits/1_rule90.v
+++ Circuits/Sequential Logic/More Circuits/1_rule90.v
@@ -1,1 +1,15 @@
+module top_module(
+    input clk,
+    input load,
+    input [511:0] data,
+    output [511:0] q ); 
+    wire [512:-1] q_tmp;
+    always@(posedge clk) begin
+        q_tmp = {1'b0, q, 1'b0};
+        for(int i = 0; i <= 511; i++)
+            q[i] = q_tmp[i + 1] ^ q_tmp[i - 1];
+        if(load)
+            q = data;
+    end
 
+endmodule
